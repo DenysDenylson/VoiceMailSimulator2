82781fbb158a19c4c5a8a0bb9ce54016d5fed3d9
DenysDenylson <DenysDenylson@gmail.com>
2016-04-07 10:25:19 -0400

    Pruebas de fallando tras poner una lista de parametros en connection

--- src/Connection.java
+++ src/Connection.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
    Connects a phone to the mail system. The purpose of this
    class is to keep track of the state of a connection, since
@@ -10,11 +13,19 @@ public class Connection
       @param s a MailSystem object
       @param p a Telephone object
    */
-   public Connection(MailSystem s, Telephone p)
+	List<UserInterface> uis;
+	
+	
+   public Connection(MailSystem s, List uis)
    {
+	  this.uis = uis;
       system = s;
-      phone = p;
       resetConnection();
+   }
+   
+   public void addUI(UserInterface ui){
+	   uis.add(ui);
+	   
    }
 
    /**
@@ -66,8 +77,15 @@ public class Connection
       currentRecording = "";
       accumulatedKeys = "";
       state = CONNECTED;
-      phone.speak(INITIAL_PROMPT);
+      
+	speakToAllUIs(INITIAL_PROMPT);
+		
    }
+
+private void speakToAllUIs(String output) {
+	for(UserInterface ui : uis) 
+		ui.speak(output);
+}
 
    /**
       Try to connect the user with the specified mailbox.
@@ -81,7 +99,9 @@ public class Connection
          if (currentMailbox != null)
          {
             state = RECORDING;
+            
             phone.speak(currentMailbox.getGreeting());
+            
          }
          else
             phone.speak("Incorrect mailbox number. Try again!");
@@ -204,10 +224,9 @@ public class Connection
    private Mailbox currentMailbox;
    private String currentRecording;
    private String accumulatedKeys;
-   private Telephone phone;
+   private UserInterface phone;
+   private UserInterface phone2;   
    private int state;
-   
-   private GUITelephone menu;
 
    private static final int DISCONNECTED = 0;
    private static final int CONNECTED = 1;
